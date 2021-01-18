7816327fc0a3e628bf79cec0329216e4cd1449a2
pethupraveen <praveenkumar.pdknp@gmail.com>
2021-01-18 20:36:01 +0530

    Update Student Page Place holders

--- application/views/Student/index.php
+++ application/views/Student/index.php
@@ -4,17 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="content">
     <div class="container-fluid">
       <!-- Table -->
-      <h2 class="mb-5">My Account Card</h2>
+      <h2 class="mb-5">Student Register</h2>
       <div class="row">
         <div class="col-md-12 col-xs-12 m-auto order-xl-1">
           <div class="card bg-secondary shadow">
             <div class="card-header bg-white border-0">
               <div class="row align-items-center">
                 <div class="col-8">
-                  <h3 class="mb-0">My account</h3>
+                  <h3 class="mb-0">Create Student</h3>
                 </div>
                 <div class="col-4 text-right">
-                  <a href="#!" class="btn btn-sm btn-primary">Settings</a>
+                  <a href="#!" class="btn btn-sm btn-primary">Add New</a>
                 </div>
               </div>
             </div>
@@ -23,30 +23,80 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <h6 class="heading-small text-muted mb-4">Student information</h6>
                 <div class="pl-lg-4">
                   <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-md-3">
                       <div class="form-group focused">
-                        <label class="form-control-label" for="input-username">Username</label>
-                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="lucky.jesse">
+                        <label class="form-control-label" for="input-username">Student_Name</label>
+                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="">
                       </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-md-3">
                       <div class="form-group">
-                        <label class="form-control-label" for="input-email">Email address</label>
-                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="jesse@example.com">
+                        <label class="form-control-label" for="input-email">Fathers_Name</label>
+                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="">
+                      </div>
+                    </div>
+                       <div class="col-md-3">
+                      <div class="form-group">
+                        <label class="form-control-label" for="input-email">Mother_Name</label>
+                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="">
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-username">Student_Blood_Group</label>
+                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="">
                       </div>
                     </div>
                   </div>
                   <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-md-3">
                       <div class="form-group focused">
-                        <label class="form-control-label" for="input-first-name">First name</label>
-                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="Lucky">
+                        <label class="form-control-label" for="input-first-name">Student_Mobile_No</label>
+                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="">
                       </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-md-3">
                       <div class="form-group focused">
-                        <label class="form-control-label" for="input-last-name">Last name</label>
-                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="Jesse">
+                        <label class="form-control-label" for="input-last-name">Student_emailid</label>
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="">
+                      </div>
+                    </div>
+                      <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Parent_Contact_No</label>
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="">
+                      </div>
+                    </div>
+                      <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Date_of_Birth</label>
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="">
+                      </div>
+                    </div>
+                  </div>
+                   <div class="row">
+                    <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-first-name">Sex</label>
+                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="">
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Community</label>
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="">
+                      </div>
+                    </div>
+                      <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Caste</label>
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="">
+                      </div>
+                    </div>
+                      <div class="col-md-3">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-last-name">Nationality</label>
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="">
                       </div>
                     </div>
                   </div>
@@ -72,27 +122,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </div>
                     <div class="col-lg-4">
                       <div class="form-group focused">
-                        <label class="form-control-label" for="input-country">Country</label>
+                        <label class="form-control-label" for="input-country">Area_Name</label>
                         <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
                       </div>
                     </div>
                     <div class="col-lg-4">
                       <div class="form-group">
-                        <label class="form-control-label" for="input-country">Postal code</label>
+                        <label class="form-control-label" for="input-country">PinCode</label>
                         <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
                       </div>
                     </div>
                   </div>
                 </div>
                 <hr class="my-4">
-                <!-- Description -->
-                <h6 class="heading-small text-muted mb-4">About me</h6>
-                <div class="pl-lg-4">
-                  <div class="form-group focused">
-                    <label>About Me</label>
-                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
-                  </div>
-                </div>
+               
+                
               </form>
             </div>
           </div>
